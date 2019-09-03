245489f00ffba4ceb0d58eb3b2ce5956d8708d39
Fabian <fabian_thuering@hotmail.com>
2019-09-04 01:28:07 +0200

    Add better comments for the example

--- example/01-saxpy/saxpy_plugin.cpp
+++ example/01-saxpy/saxpy_plugin.cpp
@@ -53,8 +53,6 @@
 // Implementation of bf_args
 #define _bf_args _bf_concat(_bf_args_, bf_id)
 
-/********************************************/
-
 /// bf_override
 #define bf_override(name) _bf_func_decl_ret name(_bf_func_decl_args)
 
@@ -71,6 +69,32 @@
 #define bf_arg_2 _bf_arg(2)
 #define bf_arg_3 _bf_arg(3)
 #define bf_arg_4 _bf_arg(4)
+#define bf_arg_5 _bf_arg(5)
+#define bf_arg_6 _bf_arg(6)
+#define bf_arg_7 _bf_arg(7)
+#define bf_arg_8 _bf_arg(8)
+#define bf_arg_9 _bf_arg(9)
+#define bf_arg_10 _bf_arg(10)
+#define bf_arg_11 _bf_arg(11)
+#define bf_arg_12 _bf_arg(12)
+#define bf_arg_12 _bf_arg(13)
+#define bf_arg_13 _bf_arg(14)
+#define bf_arg_14 _bf_arg(15)
+#define bf_arg_15 _bf_arg(16)
+#define bf_arg_16 _bf_arg(17)
+#define bf_arg_17 _bf_arg(18)
+#define bf_arg_18 _bf_arg(19)
+#define bf_arg_20 _bf_arg(20)
+#define bf_arg_21 _bf_arg(21)
+#define bf_arg_22 _bf_arg(22)
+#define bf_arg_23 _bf_arg(23)
+#define bf_arg_24 _bf_arg(24)
+#define bf_arg_25 _bf_arg(25)
+#define bf_arg_26 _bf_arg(26)
+#define bf_arg_27 _bf_arg(27)
+#define bf_arg_28 _bf_arg(28)
+#define bf_arg_29 _bf_arg(29)
+#define bf_arg_30 _bf_arg(30)
 
 /// bf_arg_name
 #define bf_arg_name(index)
@@ -100,7 +124,7 @@ static std::uint64_t s_OriginalAddresses[1];
 #define _bf_func_decl_ret_saxpy void
 #define _bf_func_decl_args_saxpy int n, float a, float *x, float *y
 
-#define _bf_func_saxpy ((void (*)(int, float, float*, float*))::s_OriginalAddresses[(std::uint64_t)::identifer::saxpy])
+#define _bf_func_saxpy ((void (*)(int, float, float *, float *))::s_OriginalAddresses[(std::uint64_t)::identifer::saxpy])
 
 #define _bf_args_saxpy n, a, x, y
 
@@ -111,25 +135,45 @@ static std::uint64_t s_OriginalAddresses[1];
 
 // ---------------
 
+//
+// 1) First, we need to define bf_id to indicate which function/method we are currently defining an override. In the following, we are going to override the
+// saxpy function a couple times and finally set up a hook for one of the versions.
+//
 #define bf_id saxpy
 
-/// 1) Call the original method
-bf_override(my_saxpy1) { return bf_call_orignal; }
+//
+// 2) In this override we are going to simply call the original function (i.e pass-through implementation).
+//
+bf_override(my_saxpy1) {
+  // bf_original expands to the function pointer of the original function, bf_args expand to the arguments which have been passed to the function.
+  return bf_original(bf_args);
+}
 
-/// 2) Call the original method (ver2)
-bf_override(my_saxpy2) { return bf_original(bf_args); }
+bf_override(my_saxpy2) {
+  // bf_call_orignal is a shorthand for the above.
+  return bf_call_orignal;
+}
 
-/// 3) Modify an argument
-bf_override(my_saxpy3) {
-  // Access argument
+//
+// 3) Here we are going to access and modify arguments.
+//
+bf_override(my_saxpy2) {
+  // There are several ways of accessing the arguments. You can either call them directly by the name ...
   auto a1 = a + 1;
+
+  // ... or use bf_arg_N where N represent the index in the argument list.
   auto a2 = bf_arg_2 + 1;
   auto a3 = bf_arg(2) + 1;
 
+  // In the end we are going to call the original function again but now with our new argument a3. Note that bf_arg can expand into multiple arguments!
   return bf_original(bf_arg(1), a3, bf_arg(3, 4));
 }
 
-class MyPlugin final : public ::bifrost::Plugin {
+//
+// 4) Here we define our plugin. The plugin provides functionality to hook functions/methods and utilities such as error handling or logging. The plugin is a
+// singleton and can be accessed via `Get<>()` anywhere.
+//
+class MySaxpyPlugin final : public ::bifrost::Plugin {
  public:
   virtual void SetUp() override {
     // bf_hook(hello_world_add, hello_world_add3)
@@ -138,4 +182,13 @@ class MyPlugin final : public ::bifrost::Plugin {
   virtual void TearDown() override {}
 };
 
-BIFROST_REGISTER_PLUGIN(MyPlugin)
+//
+// 5) Here we register our plugin - bifrost will make sure everything.
+//
+BIFROST_REGISTER_PLUGIN(MySaxpyPlugin)
+
+//
+// 6) Each plugin can define a standalone help function to easily expose it's arguments.
+//
+const char *Help() { return "saxpy example plugin."; }
+BIFROST_REGISTER_PLUGIN_HELP(Help)
