be44a89ca225cd3b1f1b9fec0d0a96c33d96ccfa
Fabian <fabian_thuering@hotmail.com>
2019-09-14 02:19:36 +0200

    Remove buffered logging

--- source/compiler/Logger/Forward.cs
+++ source/compiler/Logger/Forward.cs
@@ -23,43 +23,5 @@ namespace Bifrost.Compiler.Logger
                 logger.Sink(entry);
             }
         }
-
-        /// <summary>
-        /// Enable/Disable buffer of the log messages
-        /// </summary>
-        public void EnableBuffering(bool enable)
-        {
-            if (enable)
-            {
-                Loggers.Add(BufferTag, new Logger.Buffered());
-            }
-            else
-            {
-                Loggers.Remove(BufferTag);
-            }
-        }
-
-        /// <summary>
-        /// Flush all the buffered log messages to the registered loggers
-        /// </summary>
-        public void FlushBuffer()
-        {
-            if (!Loggers.ContainsKey(BufferTag))
-            {
-                return;
-            }
-
-            var bufferedLogger = Loggers[BufferTag] as Logger.Buffered;
-            bufferedLogger.Flush(Loggers.Where(kvp => kvp.Key != BufferTag || m_flushedLoggerTags.Contains(kvp.Key)).Select(kvp => kvp.Value));
-
-            foreach (var tag in Loggers.Keys)
-            {
-                m_flushedLoggerTags.Add(tag);
-            }
-        }
-
-        private HashSet<string> m_flushedLoggerTags = new HashSet<string>();
-
-        private static readonly string BufferTag = "__buffer__";
     }
 }
