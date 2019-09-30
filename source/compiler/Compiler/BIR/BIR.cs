67bb7a9cd2a2570ce81bd7ade02063aaadfb5479
Fabian Thuering <fthuering@nvidia.com>
2019-09-30 16:46:48 +0200

    Add BIR

--- source/compiler/Compiler/BIR/BIR.cs
+++ source/compiler/Compiler/BIR/BIR.cs
@@ -7,8 +7,58 @@ namespace Bifrost.Compiler.BIR
     /// <summary>
     /// Bifrost Intermediate Representation 
     /// </summary>
-    [Serializable]
     public class BIR
     {
+        public enum HookTypeEnum
+        {
+            Function,
+            VTable
+        }
+
+        /// <summary>
+        /// Description of an individual hook
+        /// </summary>
+        public class Hook
+        {
+            /// <summary>
+            /// Type of hook
+            /// </summary>
+            public HookTypeEnum HookType;
+
+            /// <summary>
+            /// Unique identifier of this hook (C++ enum value compatible)
+            /// </summary>
+            public string Identifier;
+
+            /// <summary>
+            /// Return type of the method/function
+            /// </summary>
+            public string ReturnType;
+
+            /// <summary>
+            /// Type of the "this" pointer (only relevant for VTable hooks)
+            /// </summary>
+            public string ThisType;
+
+            /// <summary>
+            /// Calling convention of the function
+            /// </summary>
+            public string CallingConvention;
+
+            public class Parameter
+            {
+                /// <summary>
+                /// Name of the parameter argument
+                /// </summary>
+                public string Name;
+
+                /// <summary>
+                /// Full CV-qualified type
+                /// </summary>
+                public string Type;
+            }
+            public List<Parameter> Parameters;
+        }
+        public List<Hook> Hooks;
     }
 }
